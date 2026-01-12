ff122c8deb67fe491f6d18d82bec1b7c1b609110
kirpajeet12 <kirpajeetg@gmail.com>
2026-01-12 12:11:15 -0800

    Update projects.js

--- backend/src/routes/projects.js
+++ backend/src/routes/projects.js
@@ -4,21 +4,12 @@ import { generateId } from "../utils/id.js";
 
 const router = express.Router();
 
-// Create project
 router.post("/", (req, res) => {
-  const project = {
-    id: generateId(),
-    ...req.body,
-    createdAt: new Date()
-  };
-
+  const project = { id: generateId("PRJ"), ...req.body };
   store.projects.push(project);
   res.json(project);
 });
 
-// List projects
-router.get("/", (req, res) => {
-  res.json(store.projects);
-});
+router.get("/", (_, res) => res.json(store.projects));
 
 export default router;
